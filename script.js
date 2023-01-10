6cca6c9c4102a97df35be6cb0c46a35bea86bf7e
Félix Laviéville <lavieville.felix@gmail.com>
2023-01-10 08:08:59 +0100

    various tests on drag drop events

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-//il y a
+
+
+let deck = document.querySelector('.deck');
+let pokemonStock = document.querySelector(".poke-stock");
 
 async function getData(url){
     let request = await fetch(url);
@@ -16,21 +19,46 @@ async function getPokemonData(name){
     }
 }
 
-getPokemonData('pikachu').then(r => console.log(r));
 
+//drag drop functions :
+function allowDrop(e){
+    e.preventDefault();
+}
+function dragPokemon(e){
+    e.dataTransfer.setData('pokemon', e.target.id);
+}
+function dropPokemon(e){
+    let pokemonId = e.dataTransfer.getData('pokemon');
+    e.target.appendChild(document.querySelector('#'+pokemonId));
+}
+
+//afficher les 10 premiers pokémons dans le stock
 for(let i = 1; i<31; i+=3){
+
     let img = document.createElement('img');
     let p = document.createElement('p');
+
     getPokemonData(i).then(pokemon => {
+
         img.src = pokemon['sprites']['front_default'];
+        img.draggable = false;
         p.innerHTML = pokemon['name']+" ("+pokemon['id']+")";
+
         let card = document.createElement('div');
         card.classList.add('poke-card');
         card.appendChild(img);
         card.appendChild(p);
-        card.addEventListener('dragstart', ()=>{
-            console.log('something is being dragged');
-        })
-        document.querySelector(".pokemon").appendChild(card);
+        card.id = 'pokemon_'+pokemon['id'];
+        card.draggable = true;
+        card.ondragstart = dragPokemon;
+
+        pokemonStock.appendChild(card);
+
     })
 }
+
+deck.ondragover = allowDrop;
+deck.ondrop = dropPokemon;
+
+pokemonStock.ondragover = allowDrop;
+pokemonStock.ondrop = dropPokemon;
